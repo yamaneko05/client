3f6ea16779dba70785f1508d00d26c46bcdefa6f
yamaneko05 <o.yamaneko0331@gmail.com>
2024-05-27 12:27:42 +0900

    微調整

--- src/components/bottombar.tsx
+++ src/components/bottombar.tsx
@@ -1,7 +1,7 @@
 import { SidebarMenu } from "@/components/sidebar-menu"
 import { Icon } from "@/features/users/components/icon"
 import { NavLink } from "react-router-dom"
-import { Home, User } from "lucide-react"
+import { Bell, BellDot, Home, User } from "lucide-react"
 import { useUser } from "@/features/auth/hooks/use-user"
 
 export const Bottombar = () => {
@@ -15,11 +15,18 @@ export const Bottombar = () => {
     {
       to: `/users/${loginUser?.id}`,
       icon: <User size={24} />
-    }
+    },
+    {
+      title: loginUser?.unread_notifications_count
+        ? `通知[${loginUser?.unread_notifications_count}]`
+        : "通知",
+      to: `/users/${loginUser?.id}/notifications`,
+      icon: loginUser?.unread_notifications_count ? <BellDot size={24} /> : <Bell size={24} />
+    },
   ]
 
   return loginUser && (
-    <div className="flex sm:hidden fixed bottom-0 w-full px-2 h-[60px] bg-white border-t justify-around items-center">
+    <div className="z-10 flex sm:hidden fixed bottom-0 w-full px-2 h-[60px] bg-white border-t justify-around items-center">
       {navLinks.map((navLink, index) => (
         <NavLink to={navLink.to} key={index} className="p-3">
           {navLink.icon}
--- src/features/notifications/routes/notifications.tsx
+++ src/features/notifications/routes/notifications.tsx
@@ -2,19 +2,22 @@ import { PageHeading } from "@/components/ui/page-heading";
 import { NotificationsList } from "@/features/notifications/components/notifications-list";
 import { NotificationType } from "@/features/notifications/types";
 import { api } from "@/lib/api";
-// import { queryClient } from "@/lib/queryClient";
+import { queryClient } from "@/lib/queryClient";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 
 export const NotificationsRoute = () => {
   const { userId } = useParams();
+
+  const invalidate = () => queryClient.invalidateQueries({queryKey: ["loginUser"]})
   
   const { data: notifications } = useQuery({
     queryKey: ["notifications", userId],
-    queryFn: () => api.get<NotificationType[]>(`/users/${userId}/notifications`).then(res => res.data)
+    queryFn: () => api.get<NotificationType[]>(`/users/${userId}/notifications`).then(res => {
+      invalidate()
+      return res.data
+    })
   })
-
-  // const invalidate = () => queryClient.invalidateQueries({queryKey: ["notifications", userId]})
 
   return notifications ? (
     <div className="">
